aafdd05eec5a88e4ec3c4d30589855bf9f941182
rony <26e4u69@gmail.com>
2023-11-25 16:32:30 +0200

    States game start

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print('not implemented...')
+import turtle
+
+
+
+
+screen = turtle.Screen()
+screen.title('U.S. States Game')
+image = "blank_states_img.gif"
+screen.addshape(image)
+turtle.shape(image)
+
+
+answer_state = screen.textinput('Guess the state', "Another a state's name")
+# def get_coor(x,y):
+#     print(x,y)
+# turtle.onscreenclick(get_coor)
+
+
+turtle.mainloop()
