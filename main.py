1caa9b9b1094c89646f11db78250f07ca0650200
rony <26e4u69@gmail.com>
2023-11-25 19:00:44 +0200

    States game - before Building a class to place states name on the map

--- main.py
+++ main.py
@@ -1,16 +1,37 @@
 import turtle
-
+import pandas
 
 
 
 screen = turtle.Screen()
+guessed = 0
 screen.title('U.S. States Game')
 image = "blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
 
 
-answer_state = screen.textinput('Guess the state', "Another a state's name")
+
+states = pandas.read_csv('50_states.csv')
+states_names_list = states['state'].to_list()
+
+game = True
+while game:
+    answer_state = screen.textinput(f'Guess the state {guessed}/50', "Another a state's name").title()
+    if answer_state in states_names_list:
+        state_row = states[states.state == answer_state]
+        x_cor = state_row.x
+        y_cor = state_row.y
+        guessed += 1
+
+
+
+
+
+
+
+
+
 # def get_coor(x,y):
 #     print(x,y)
 # turtle.onscreenclick(get_coor)
